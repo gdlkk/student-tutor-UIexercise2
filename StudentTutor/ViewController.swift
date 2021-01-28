e636097d5daed31a8c9a001fa5c755a8f2732006
gdlkk <mateusz.d.mateusz@gmail.com>
2021-01-28 23:54:45 +0100

    Initial commit

--- StudentTutor/ViewController.swift
+++ StudentTutor/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageView: UIImageView = {
+           let imageView = UIImageView(frame: .zero)
+           imageView.image = UIImage(named: "background")
+           imageView.contentMode = .scaleToFill
+           imageView.translatesAutoresizingMaskIntoConstraints = false
+           return imageView
+       }()
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        view.insertSubview(imageView, at: 0)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 
 
